12827e6681f9be09eba15add98e304a2b98be5cb
Allan Edwards <allanedwards@Allans-MacBook-Pro.local>
2018-02-05 09:41:42 -0500

    Firebase setup

---
+++ devslopes-social/Controller/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  devslopes-social
+//
+//  Created by Allan Edwards on 2/5/18.
+//  Copyright © 2018 Allan Edwards. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
